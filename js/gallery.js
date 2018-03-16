349db8322f9ee4b5c309d9ed29acb39a0da9f1be
LukasZvikas <lzvikas1@gmail.com>
2018-03-15 23:46:04 -0500

    Part2 1-3

--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,16 @@
 // requestAnim shim layer by Paul Irish
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       || 
-              window.webkitRequestAnimationFrame || 
-              window.mozRequestAnimationFrame    || 
-              window.oRequestAnimationFrame      || 
-              window.msRequestAnimationFrame     || 
-              function(/* function */ callback, /* DOMElement */ element){
-                window.setTimeout(callback, 1000 / 60);
-              };
-    })();
-  
+window.requestAnimFrame = (function() {
+  return (
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(/* function */ callback, /* DOMElement */ element) {
+      window.setTimeout(callback, 1000 / 60);
+    }
+  );
+})();
 
 // example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
 
@@ -18,26 +19,26 @@ animate();
 var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
-    requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+  requestAnimFrame(animate);
+  var currentTime = new Date().getTime();
+  if (mLastFrameTime === 0) {
+    mLastFrameTime = currentTime;
+  }
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+  if (currentTime - mLastFrameTime > mWaitTime) {
+    swapPhoto();
+    mLastFrameTime = currentTime;
+  }
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	console.log('swap photo');
+  //Add code here to access the #slideShow element.
+  //Access the img element and replace its source
+  //with a new image from your images array which is loaded
+  //from the JSON string
+  console.log("swap photo");
 }
 
 // Counter for the mImages array
@@ -45,6 +46,29 @@ var mCurrentIndex = 0;
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
+var url = "images.json";
+mRequest.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    var images = JSON.parse(this.responseText);
+
+    var mImages = [];
+ 
+    $.each(images, function(key, value) {
+      var img = value;
+
+      GalleryImage(img);
+
+      mImages.push(img);
+    });
+
+    console.log(mImages);
+ 
+    return mImages;
+  }
+};
+
+mRequest.open("GET", url, true);
+mRequest.send();
 
 // Array holding GalleryImage objects (see below).
 var mImages = [];
@@ -54,35 +78,43 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
-
+var mUrl = "insert_url_here_to_image_json";
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
+  return function(e) {
+    galleryImage.img = e.target;
+    mImages.push(galleryImage);
+  };
 }
 
-$(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	
+$(document).ready(function() {
+  // This initially hides the photos' metadata information
+  $(".details")
+    .eq(0)
+    .hide();
 });
 
-window.addEventListener('load', function() {
-	
-	console.log('window loaded');
+window.addEventListener(
+  "load",
+  function() {
+    console.log("window loaded");
+  },
+  false
+);
 
-}, false);
+function GalleryImage(image) {
+  return {
+    location: image.imgLocation,
+    description: image.description,
+    date: image.date,
+    imageSrc: image.imgPath
+  };
 
-function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+  //implement me as an object to hold the following data about an image:
+  //1. location where photo was taken
+  //2. description of photo
+  //3. the date when the photo was taken
+  //4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
 }
